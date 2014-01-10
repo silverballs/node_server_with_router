aa60f10eba9cc57ff13e41b34537b62eba063c50
Silverballs <asilbersmith11@gmail.com>
2014-01-10 06:22:47 -0500

    Add start function and onRequest

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var http = require("http");
 
-http.createServer(function(request, response){
-	response.writeHead(200, {"Content-Type": "Text/plain"});
-	response.write("Hello World");
-	response.end();
+function start() {
+  function onRequest(request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
 
-}).listen(8888);
-
- http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
 }
 
 exports.start = start;
+
